aa9502fe275c4af337b2cbd5054cade796845d61
Cole Westbrook <colewestbrookphoto@gmail.com>
2023-10-30 09:01:51 -0400

    Added Category grid

---
+++ src/components/category-grid/category-grid.component.jsx
@@ -0,0 +1,14 @@
+import "./category-grid.styles.scss";
+import CategoryItem from "../category-item/category-item.component";
+
+const CategoryGrid = ({ categories }) => {
+  return (
+    <div className="categories-container">
+      {categories.map((category) => {
+        return <CategoryItem key={category.id} category={category} />;
+      })}
+    </div>
+  );
+};
+
+export default CategoryGrid;
